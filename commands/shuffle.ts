7973d409f053c7d85bbe67023b0535b3d92f9dcd
Alyssa P. Hacker <alyssa@example.com>
2022-12-18 22:38:12 -0800

    Adding shuffle.ts file

---
+++ commands/shuffle.ts
@@ -0,0 +1,21 @@
+import { SlashCommandBuilder, GuildMember } from 'discord.js';
+
+import { createCommand } from './command';
+
+export const shuffle = createCommand({
+    data: new SlashCommandBuilder()
+        .setName('shuffle')
+        .setDescription('Shuffles the list of songs')
+        .toJSON(),
+    async execute(interaction, player) {
+        const member = interaction.member as GuildMember;
+        const channel = member.voice.channel;
+        if (channel == null) {
+            await interaction.reply('You need to be in a voice channel to summon me, dumbass.');
+            return;
+        }
+        
+        await player.shuffle(channel);
+        await interaction.reply(`Scramblin time`);
+    }
+});
