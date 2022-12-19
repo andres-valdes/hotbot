e460c7a7f08b1061250ac9d68c827c84b7103a24
Alyssa P. Hacker <alyssa@example.com>
2022-12-18 20:38:13 -0800

    Adding url option to play

--- commands/play.ts
+++ commands/play.ts
@@ -3,20 +3,73 @@ import { SlashCommandBuilder, GuildMember } from 'discord.js';
 import { createCommand } from './command';
 import { getPlayer } from '../components/player';
 
+declare enum subcommands {
+    search = 'search',
+    url = 'url',
+}
+
 export const play = createCommand({
     data: new SlashCommandBuilder()
         .setName('play')
         .setDescription('Play "Biggie Smalls feat. Thomas the Tank Engine"')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName(subcommands.search)
+                .setDescription('Searches for a song')
+                .addStringOption(
+                    option =>
+                        option
+                            .setName('search-terms')
+                            .setDescription('A list of keywords used to search youtube for a video')
+                            .setRequired(true),
+                )
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName(subcommands.url)
+                .setDescription('Link a specific song or playlist')
+                .addStringOption(
+                    option =>
+                        option
+                            .setName('url')
+                            .setDescription('A link to the song or playlist to play')
+                            .setRequired(true),
+                )
+        )
         .toJSON(),
     async execute(interaction) {
         const member = interaction.member as GuildMember;
         const channel = member.voice.channel;
+        const url = interaction.options.getString('url');
+        console.log(interaction.options);
+
         if (channel == null) {
             await interaction.reply('You need to be in a voice channel to summon me, dumbass.');
             return;
         }
-        await (await getPlayer()).play(channel, 'https://www.youtube.com/watch?v=ETfiUYij5UE');
-        
+
+        let player_arguments = null;
+        switch (interaction.options.getSubcommand.name) {
+            case subcommands.search: {
+                player_arguments = interaction.options.getString(subcommands.search);
+                break;
+            }
+            case subcommands.url: {
+                const maybe_url = interaction.options.getString(subcommands.search);
+                if (maybe_url == null) {
+                    return;
+                }
+                player_arguments = new URL(maybe_url).toString();
+                break;
+            }
+        }
+
+        if (player_arguments == null) {
+            return;
+        }
+
+        await (await getPlayer()).play(channel, player_arguments);
+
         await interaction.reply('playing stuff');
     }
 });
