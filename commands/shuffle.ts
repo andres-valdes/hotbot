06aa0e54b0124154106be65fc56a9f5aa7dceaac
andres-valdes <andres.ernesto.valdes@gmail.com>
2022-12-18 22:46:53 -0800

    fix type error

--- commands/shuffle.ts
+++ commands/shuffle.ts
@@ -7,14 +7,13 @@ export const shuffle = createCommand({
         .setName('shuffle')
         .setDescription('Shuffles the list of songs')
         .toJSON(),
-    async execute(interaction, {player}) {
+    async execute(interaction, { player }) {
         const member = interaction.member as GuildMember;
         const channel = member.voice.channel;
         if (channel == null) {
             await interaction.reply('You need to be in a voice channel to summon me, dumbass.');
             return;
         }
-        
         await player.shuffle(channel);
         await interaction.reply(`Scramblin time`);
     }
